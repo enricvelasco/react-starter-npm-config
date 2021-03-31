97018d85e541d34e28db9abc7eb73c43841c963a
enric <enric.velasco.g@gmail.com>
2021-03-31 10:07:47 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
+import Title from './components/npm-component'
 
 const App = () => (
-  <div>
-    <h1>Hi this is React Starter!!</h1>
-    <p>This is an alternative to 'create-react-app'</p>
-    <p>remember that you owe this project has no style</p>
-    <p>we recommend that you use styled-components <a href='https://www.npmjs.com/package/styled-components' target='_blank' rel='noopener noreferrer'>https://www.npmjs.com/package/styled-components</a></p>
-  </div>
+  <Title text='Este es mi componente (TITLE)' />
 )
 
 export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,82 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
-module.exports = {
-  devServer: {
-    port: 9000
-  },
-  output: {
-    filename: 'app.bundle.js',
-    publicPath: '/'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+module.exports = env => {
+  console.log('ENV:::', env.MODE)
+  const fileEntry = env.MODE === 'development' ? './src/components/npm-component/index.js' : './src/index.js'
+
+  console.log('FILE_ENTRY', fileEntry)
+  return({
+    devServer: {
+      port: 9000
+    },
+    entry: fileEntry,
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'app.bundle.js',
+      publicPath: '/'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
           }
         }
-      }
-    ]
-  }
+      ]
+    }
+  })
 }
+
+/*
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = env => {
+  console.log('ENV:::', env.MODE)
+  const fileEntry = env.MODE === 'development' ? './src/App.js' : './src/index.js'
+
+  console.log('FILE_ENTRY', fileEntry)
+  return({
+    devServer: {
+      port: 9000
+    },
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, './build'),
+      filename: 'index.js',
+      publicPath: '/'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        }
+      ]
+    }
+  })
+}
+
+ */
